3a8a940f678373abf6b72b937c7da42dbb94c620
JerryKebede <eyerusalem_ke@yahoo.com>
2024-06-25 06:35:17 -0700

    done

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,35 +1,26 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
+"""script that starts a Flask web application"""
 
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'.
-    /hbnb: Displays 'HBNB'.
-    /c/<text>: Displays 'C' followed by the value of <text>.
-"""
 from flask import Flask
-
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'."""
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    """def to say Hello HBNB with use of slash"""
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """Displays 'HBNB'."""
-    return "HBNB"
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """def to say HBNB with use of /hbnb"""
+    return 'HBNB'
 
 
-@app.route("/c/<text>", strict_slashes=False)
+@app.route('/c/<text>', strict_slashes=False)
 def c(text):
-    """Displays 'C' followed by the value of <text>."""
-    text = text.replace("_", " ")
-    return "C {}".format(text)
+    """def to display C and text after"""
+    return 'C %s' % text.replace('_', ' ')
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
